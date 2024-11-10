7fef201ca42025706086c1b5b9132cce9d1bddb9
simon <simonkardell@gmail.com>
2024-11-10 13:46:33 +0100

    initial commit

---
+++ src/lambda.ts
@@ -0,0 +1,81 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { ValidationFunc, Request } from "./validation";
+import { Result, StatusError } from "@kardell/result";
+import pino from "pino";
+
+const baseLogger = pino();
+
+type Response = {
+  statusCode: number;
+  body: string;
+};
+
+type ErrorMessage = {
+  message: string;
+  details: string | undefined;
+};
+
+type LambdaInput<T> = {
+  userID: string | undefined;
+  requestID: string;
+  logger: pino.Logger;
+  data: T;
+};
+
+type Handler = (event: APIGatewayProxyEvent) => Promise<Response>;
+
+const apiGatewayHandler = <T extends Request<unknown, unknown, unknown>, V>(
+  validator: ValidationFunc<T>,
+  func: (req: LambdaInput<T>) => Promise<Result<V>>
+): Handler => {
+  return async (event: APIGatewayProxyEvent): Promise<Response> => {
+    const logger = baseLogger.child({
+      requestID: event.requestContext.requestId,
+    });
+    event.requestContext.extendedRequestId;
+    const input: Request<unknown, unknown, unknown> = {
+      body: event.body,
+      params: event.pathParameters,
+      query: event.queryStringParameters,
+    };
+    const { data: request, error } = validator(input);
+    if (error) {
+      return {
+        statusCode: error.status,
+        body: JSON.stringify(<ErrorMessage>{
+          message: error.message,
+          details: error.details,
+        }),
+      };
+    }
+    try {
+      const input: LambdaInput<T> = {
+        userID: event.requestContext.authorizer?.iam.cognitoIdentity.identityId,
+        requestID: event.requestContext.requestId,
+        data: request,
+        logger,
+      };
+      const res = await func(input);
+      return res.fold<Response, Response>(
+        (value) => ({
+          statusCode: 200,
+          body: JSON.stringify(value),
+        }),
+        (e) => ({
+          statusCode: e.status,
+          body: JSON.stringify({ message: e.message }),
+        })
+      );
+    } catch (e) {
+      logger.error(`Uncontrolled error caught in wrapper ${e}`);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: StatusError.Internal().message }),
+      };
+    }
+  };
+};
+
+export const handlers = {
+  apiGateway: apiGatewayHandler,
+};
---
+++ src/validation.ts
@@ -0,0 +1,67 @@
+import { Result, StatusError } from "@kardell/result";
+import Joi, { SchemaLike } from "joi";
+
+export type Request<P = unknown, B = unknown, Q = unknown> = {
+  params?: P;
+  body?: B;
+  query?: Q;
+};
+
+export type APISchema<P = any, B = any, Q = any> = {
+  params?: { [index in keyof P]: SchemaLike };
+  body?: { [index in keyof B]: SchemaLike };
+  query?: { [index in keyof Q]: SchemaLike };
+};
+
+const validate = (data: unknown, schema?: Joi.AnySchema): Result<any> => {
+  if (!schema) {
+    return Result.of(data);
+  }
+  const res = schema.validate(data);
+  if (res.error) {
+    return Result.failure(
+      StatusError.BadRequest().withDetails(res.error.message)
+    );
+  }
+  return Result.of(res.value);
+};
+
+export type ValidationFunc<T> = (
+  data: Request<unknown, unknown, unknown>
+) => Result<T>;
+
+export const validator = <P = any, B = any, Q = any>(
+  pmsSchema: APISchema<P, B, Q>
+): ValidationFunc<Request<P, B, Q>> => {
+  const schema = {
+    params: pmsSchema.params ? Joi.object(pmsSchema.params) : Joi.any(),
+    body: pmsSchema.body ? Joi.object(pmsSchema.body) : Joi.any(),
+    query: pmsSchema.query ? Joi.object(pmsSchema.query) : Joi.any(),
+  };
+  return <T extends Request<unknown, unknown, unknown>>(
+    data: Request<unknown, unknown, unknown>
+  ): Result<T> => {
+    const { data: body, error: bodyError } = validate(data.body, schema.body);
+    if (bodyError) {
+      console.log("bodyError", bodyError);
+      return Result.failure(bodyError);
+    }
+
+    const { data: params, error: paramsError } = validate(
+      data.params,
+      schema.params
+    );
+    if (paramsError) {
+      return Result.failure(paramsError);
+    }
+
+    const { data: query, error: queryError } = validate(
+      data.query,
+      schema.query
+    );
+    if (queryError) {
+      return Result.failure(queryError);
+    }
+    return Result.of({ body, params, query } as T);
+  };
+};
